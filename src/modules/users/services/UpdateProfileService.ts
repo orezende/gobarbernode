5da81637e7d1befcd6319c16edb74e5c02617fc0
joaorabelo98 <joaorabello99@gmail.com>
2020-07-22 17:38:22 -0300

    finish user email

--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -1,15 +1,15 @@
 import { injectable, inject } from 'tsyringe';
 
-// import User from '@modules/users/infra/typeorm/entities/User';
+import User from '@modules/users/infra/typeorm/entities/User';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
   id: string;
-  name?: string;
-  email?: string;
+  name: string;
+  email: string;
   password?: string;
   oldPassword?: string;
 }
@@ -24,5 +24,22 @@ export default class UpdateProfileService {
     private hashProvider: IHashProvider,
   ) {}
 
-  public async execute({ id, name, email }: IRequest): Promise<void> {}
+  public async execute({ id, name, email }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(id);
+
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    const userWithUpdatedEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id != id) {
+      throw new AppError('E-mail already in use');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    return this.usersRepository.save(user);
+  }
 }
--- src/modules/users/services/tests/UpdateProfileService.spec.ts
+++ src/modules/users/services/tests/UpdateProfileService.spec.ts
@@ -1,5 +1,62 @@
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
+import UpdateProfileService from '../UpdateProfileService';
+
 describe('UpdateProfile context', () => {
+  let fakeUsersRepository: FakeUsersRepository;
+  let fakeHashProvider: FakeHashProvider;
+  let updateProfileService: UpdateProfileService;
+
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+
+    updateProfileService = new UpdateProfileService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+  });
+
   it('should be able to update a user profile', async () => {
-    expect(1 + 1).toBe(2);
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '123456',
+    });
+
+    const updatedUser = await updateProfileService.execute({
+      id: user.id,
+      name: 'John Trê',
+      email: 'johntre@mail.com',
+    });
+
+    expect(updatedUser).toMatchObject({
+      id: user.id,
+      name: 'John Trê',
+      email: 'johntre@mail.com',
+    });
+  });
+
+  it('should not be able to change to another user email', async () => {
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '123456',
+    });
+
+    const user = await fakeUsersRepository.create({
+      name: 'Cleitun',
+      email: 'teste@mail.com',
+      password: '123456',
+    });
+
+    expect(
+      updateProfileService.execute({
+        id: user.id,
+        name: 'John Trê',
+        email: 'johndoe@mail.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
