7c81a94d469630664f9b90d5b37239211b835989
joaorabelo98 <joaorabello99@gmail.com>
2020-07-25 01:11:16 -0300

    add celebrate lib

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+// import { celebrate, Segments } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
